e4cc1c73729d7dc160feffcb5480babf60fa43c8
Bayardd <bayard.maniraho@gmail.com>
2019-12-20 17:48:05 +0100

    Re starting everything

---
+++ README.md
@@ -0,0 +1,8 @@
+This application is developped for the 7th project in the developper path for Openclassrooms.
+
+The app will be using the following stack :
+- Python
+- Flask
+- HTML/CSS
+- Javascript
+- Bootstrap
--- test/test_parser.py
+++
@@ -1,7 +0,0 @@
-from app.fonctions import parserKiller
-
-def test_parserKiller():
-
-    result = parserKiller("Salut GrandPY peux tu me donner l'adresse d'Openclassrooms ?")
-    assert result == 'adresse openclassrooms'
-
