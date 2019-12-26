d4f85e22765ba148258fd0ab7d23bf6c423425c7
Bayardd <bayard.maniraho@gmail.com>
2019-12-26 02:47:11 +0100

    bootstrap initialized

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
+from flask import render_template
 from app import app
 
 @app.route('/') #Decorator which binds the url given ('/') to the function home.
 @app.route('/home') #When a browser request this URL, FLASK give the return value as RESPONSE.
 def home():
-    return "Hello World"
+    return render_template('home.html', title = 'Grandpy The Bot')
