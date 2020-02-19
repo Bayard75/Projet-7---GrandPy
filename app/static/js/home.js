d0039a917d2cd039ab8438b41977137d4fe608aa
Bayardd <bayard.maniraho@gmail.com>
2020-02-19 17:39:56 +0100

    map succefully added with marker

--- app/static/js/home.js
+++ app/static/js/home.js
@@ -1,7 +1,23 @@
+
+function showMap(longitude, latitude)
+{
+  let location ={lat: latitude, lng: longitude};
+  let options_map = {
+    zoom : 15,
+    center : location
+  };
+  let divMap = document.getElementById("map");
+  let map = new google.maps.Map(divMap, options_map);
+  let marker = new google.maps.Marker({position : location, map:map});
+
+}
+
+
+
 function fetching(){
       let body = {question : document.getElementById("value").value};
       let myHeaders = new Headers();
-      myHeaders.append("COntent-Type","application/json"); //Important or request.get_json() return None 
+      myHeaders.append("Content-Type","application/json"); //Important or request.get_json() return None 
 
       fetch('/submit', {
         // Specify the method
@@ -19,6 +35,7 @@ function fetching(){
 
         let adresse = document.getElementById("adresse");
         adresse.innerHTML = data["adresse"];
+        setTimeout(showMap(data["longitude"],data["latitude"]), 1000000);
       });
       }
 
