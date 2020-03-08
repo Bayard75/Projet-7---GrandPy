6f38fa24e584914745b3c3c6469b6bcaadd903cc
Bayardd <bayard.maniraho@gmail.com>
2020-03-08 23:46:31 +0100

    revamped front

--- app/static/js/home.js
+++ app/static/js/home.js
@@ -1,24 +1,33 @@
 
+let chatbox = document.getElementById('chatbox');
+
+
+
 function showMap(longitude, latitude)
 {
+  console.log('showmap ')
   let location ={lat: parseFloat(latitude), lng: parseFloat(longitude)};
   let options_map = {
     zoom : 15,
     center : location
   };
-
-  let divMap = document.getElementById("map");
-  let map = new google.maps.Map(divMap, options_map);
+  let createDivMap = document.createElement('div');
+  createDivMap.setAttribute('class','map');
+  chatbox.appendChild(createDivMap);
+  let divMap = document.getElementsByClassName('map')
+  size = divMap.length;
+  console.log(size)
+  let map = new google.maps.Map(divMap[size-1], options_map);
   let marker = new google.maps.Marker({position : location, map:map});
   //Add info on the marker
 }
 
 
 
-function fetching(){
+function fetching()
+{
       
       // I should set the animation here before the post request 
-
       let body = {question : document.getElementById("value").value};
       let myHeaders = new Headers();
       myHeaders.append("Content-Type","application/json"); //Important or request.get_json() returns None 
@@ -31,30 +40,38 @@ function fetching(){
         headers: myHeaders
       })
       .then(function (response) { // At this point, Flask dealted with the question
-        let question = document.getElementById("question")
-        question.innerHTML = body["question"];
+        let user_input = document.getElementById("value");
+        let question = document.createElement('p')
+        
+        question.setAttribute('class','user');
+        console.log(user_input.value)
+        chatbox.appendChild(question);
+        question.innerHTML = user_input.value;
+        user_input.value='';
+        console.log(response.status)
         return response.json();
       })
       .then(function (data) {
-
-        let adresse = document.getElementById("adresse");
-        let histoire = document.getElementById("histoire");
         // I should remove the animation about here as we have our data 
-
-        adresse.innerHTML = data["adresse"];
-        let longitude = data["longitude"];
-        let latitude = data["latitude"];
+        let returned_adress = document.createElement('p');
+        let history = document.createElement('p');
+        returned_adress.setAttribute('class','bot');
+        history.setAttribute('class','bot');
         console.log(data);
 
-        showMap(longitude,latitude);
-        histoire.innerHTML = data["summary"];
-
+        chatbox.appendChild(returned_adress);
+        returned_adress.innerHTML = data['adresse'];
+        showMap(data["longitude"],data["latitude"]);
+        chatbox.appendChild(history)
+        history.innerHTML = data["summary"];
+      
         //Add catch !
-      });
-      }
-
-      let submit = document.getElementById("form");
-      submit.addEventListener("submit",function(event){
-        fetching();
-        event.preventDefault();
       })
+}
+
+let submit = document.getElementById("form");
+submit.addEventListener("submit",function(event)
+{
+    fetching();
+    event.preventDefault();
+})
