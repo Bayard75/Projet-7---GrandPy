8d29e3cbeb82f332c25e614a80da9f84345cf722
Bayard Maniraho <48485930+Bayard75@users.noreply.github.com>
2023-03-31 21:01:52 +0200

    Add files via upload

--- run.py
+++ run.py
@@ -1,5 +1,3 @@
 from app import app #From the package app we import the variable app
-from flask_sqlalchemy import SQLAlchemy
-app.config['SQLALCHEY_DATABSE_URI'] = 'sqlite:///site'
 if __name__== "__main__":
     app.run(debug=False)
